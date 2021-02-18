9cce7491a6cda8bafd6fc9f88bd213629437d6d7
cean_park <bagsihyeon@bagsihyeon-ui-MacBookPro.local>
2021-02-18 23:54:31 +0900

    add ds page style

--- src/pages/DsPage/DsPage.js
+++ src/pages/DsPage/DsPage.js
@@ -4,54 +4,58 @@ import style from "./DsPage.module.scss";
 const DsPage = () => {
     return (
         <div className={style.page__box}>
-            <div className={style.page__title}>
+            <div className={style.ds__title}>
                 Design System
             </div>
-            <div className={style.page__contents}>
+            <div className={style.ds__contents}>
                 <div className={style.ds__color_palette}>
-                    <div>
+                    <div className={style.ds__subtitle}>
                         color palette
                     </div>
-                    <div className={style.ds__color_box}>
-                        <div className={style.ds__color_box__offset} />
-                        <div className={style.ds__color_box__name}>
-                            #f6f4f1
+                    <div className={style.ds__color_box_set}>
+                        <div className={style.ds__color_box}>
+                            <div className={`${style.ds__color_box__offset} ${style.ds__color_box_main}`} />
+                            <div className={style.ds__color_box__name}>
+                                #f6f4f1
+                            </div>
                         </div>
-                    </div>
-                    <div className={style.ds__color_box}>
-                        <div className={style.ds__color_box__offset} />
-                        <div className={style.ds__color_box__name}>
-                            #262626
+                        <div className={style.ds__color_box}>
+                            <div className={`${style.ds__color_box__offset} ${style.ds__color_box_text}`} />
+                            <div className={style.ds__color_box__name}>
+                                #262626
+                            </div>
                         </div>
-                    </div>
-                    <div className={style.ds__color_box}>
-                        <div className={style.ds__color_box__offset} />
-                        <div className={style.ds__color_box__name}>
-                            #7a6661
+                        <div className={style.ds__color_box}>
+                            <div className={`${style.ds__color_box__offset} ${style.ds__color_box_cta}`} />
+                            <div className={style.ds__color_box__name}>
+                                #7a6661
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className={style.ds__font_box}>
-                    <div className={style.ds__font_title}>
+                    <div className={style.ds__subtitle}>
                         font style
                     </div>
-                    <div className={style.ds__font_box__title}>
-                        Title
-                    </div>
-                    <div className={style.ds__font_box__subtitle}>
-                        SubTitle
-                    </div>
-                    <div className={style.ds__font_box__body}>
-                        Body
-                    </div>
-                    <div className={style.ds__font_box__title}>
-                        타이틀
-                    </div>
-                    <div className={style.ds__font_box__subtitle}>
-                        서브타이틀
-                    </div>
-                    <div className={style.ds__font_box__body}>
-                        본문
+                    <div className={style.ds__font_box_set}>
+                        <div className={style.ds__font_box__title}>
+                            Simple and fresh and fair from winter's close emerging,
+                        </div>
+                        <div className={style.ds__font_box__subtitle}>
+                            Simple and fresh and fair from winter's close emerging,
+                        </div>
+                        <div className={style.ds__font_box__body}>
+                            Simple and fresh and fair from winter's close emerging,
+                        </div>
+                        <div className={style.ds__font_box__title}>
+                            동해물과 백두산이 마르고 닳도록
+                        </div>
+                        <div className={style.ds__font_box__subtitle}>
+                            동해물과 백두산이 마르고 닳도록
+                        </div>
+                        <div className={style.ds__font_box__body}>
+                            동해물과 백두산이 마르고 닳도록
+                        </div>
                     </div>
                 </div>
             </div>
